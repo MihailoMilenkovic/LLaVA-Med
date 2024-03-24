fcb017587ff9bfefa2b0d6826c1bb77b16e63527
Mihailo Milenkovic <mmilenkovic@groq.com>
2024-03-24 10:48:59 -0700

    add working training scripts and checkpoint loading

---
+++ scripts/apply_llava_delta.sh
@@ -0,0 +1,4 @@
+python3 -m llava.model.apply_delta \
+    --base huggyllama/llama-7b \
+    --target /home/mmilenkovic/llava-weights-og \
+    --delta microsoft/llava-med-7b-delta
---
+++ scripts/train_vqa_rad.sh
@@ -0,0 +1,35 @@
+script_dir=$(dirname "$(readlink -f "$0")")
+par_dir=$(dirname "$script_dir")
+torchrun --nnodes=1 --nproc_per_node=1 --master_port=25001 \
+    $par_dir/llava/train/train_mem.py \
+    --model_name_or_path /home/mmilenkovic/llava-weights-og \
+    --data_path /home/mmilenkovic/git/LLaVA-Med/data/vqa_rad/train/train.json \
+    --image_folder /home/mmilenkovic/git/LLaVA-Med/data/vqa_rad/train/images \
+    --vision_tower openai/clip-vit-large-patch14 \
+    --mm_vision_select_layer -2 \
+    --mm_use_im_start_end True \
+    --tune_mm_mlp_adapter True \
+    --bf16 True \
+    --output_dir /home/mmilenkovic/llava-weights-finetuned-vqa-rad \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
+    --evaluation_strategy "no" \
+    --save_strategy "steps" \
+    --save_steps 500 \
+    --save_total_limit 3 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --tf32 True \
+    --model_max_length 1024 \
+    --gradient_checkpointing True \
+    --lazy_preprocess True \
+    --report_to wandb    
+
+    # --pretrain_mm_mlp_adapter /home/chunyl/research/models/llava/LLaVA-13b-pretrain-projector-v0/LLaVA-13b-pretrain-projector-v0-CC3M-595K-original_caption.bin \
+    # --fsdp "full_shard auto_wrap" \
+    # --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
