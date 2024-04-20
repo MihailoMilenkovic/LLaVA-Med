50fa4697ab9bb6fdaf8342242a01fdd8c9b35826
Mihailo Milenkovic <mmilenkovic@groq.com>
2024-04-20 10:46:37 -0700

    add eval script

---
+++ scripts/eval_vqa_rad.sh
@@ -0,0 +1,26 @@
+script_dir=$(dirname "$(readlink -f "$0")")
+par_dir=$(dirname "$script_dir")
+weights_path_finetuned=/home/mmilenkovic/llava-weights-finetuned-vqa-rad 
+weights_path_og=/home/mmilenkovic/llava-weights-og
+use_finetuned_model=true
+
+if [ "$use_finetuned_model" = "true" ]; then
+    weights_path="$weights_path_finetuned"
+else
+    weights_path="$weights_path_og"
+fi
+
+echo "loading weights from $weights_path"
+echo "saving answers to $answer_dir"
+
+answer_dir="/home/chunyl/research/output/llava/results/instruct_vqa_coco_val2014/answer-file.jsonl"
+test_question_folder="/home/mmilenkovic/git/LLaVA-Med/data/vqa_rad/test"
+test_question_file="$test_question_folder/test.json"
+test_image_path="$test_question_folder/images"
+
+python $par_dir/llava/eval/model_vqa.py \
+    --model-name $weights_path\
+    --question-file $test_question_file \
+    --image-folder $test_image_path \
+    --answers-file $answer_dir 
+   
